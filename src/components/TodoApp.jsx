3180adb3bbfac70d10f66bbe1ae033b2d1767c70
knuspersnack <thumos@posteo.gr>
2021-02-10 20:19:41 +0100

    Routing added + ToDo List

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 class TodoApp extends Component {
   render() {
     return (
-      <div className="">
-        <LoginComponent />
-        <WelcomeComponent />
+      <div className="TodoApp">
+        <Router>
+          <>
+            <Switch>
+              <Route path="/" exact component={LoginComponent} />
+              <Route path="/login" component={LoginComponent} />
+              <Route path="/welcome/:name" component={WelcomeComponent} />
+              <Route path="/todos" component={ListTodosComponent} />
+              <Route component={ErrorComponent} />
+            </Switch>
+          </>
+        </Router>
       </div>
     )
   }
@@ -32,9 +42,7 @@ class LoginComponent extends Component {
 
   loginClicked() {
     if (this.state.username === 'in28minutes' && this.state.password === 'test') {
-      console.log('Successful');
-      this.setState({ showSuccessMessage: true });
-      this.setState({ hasLoginFailed: false });
+      this.props.history.push(`/welcome/${this.state.username}`);
     } else {
       this.setState({ showSuccessMessage: false });
       this.setState({ hasLoginFailed: true });
@@ -62,9 +70,81 @@ class LoginComponent extends Component {
 
 class WelcomeComponent extends Component {
   render() {
-    return <div>Welcome to in28Minutes</div>
+    // The parameter from the URL will be read 
+    return <>
+      <span> Welcome {this.props.match.params.name}. </span>
+      <span>You can manage your todos <Link to="/todos">here</Link></span>
+    </>
   }
 }
+
+function ErrorComponent() {
+  return <div>An error occured!</div>
+}
+
+
+class ListTodosComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos:
+        [
+          {
+            id: 1,
+            description: 'Learn React',
+            done: false,
+            targetDate: new Date()
+          },
+          {
+            id: 2,
+            description: 'Visit React',
+            done: true,
+            targetDate: new Date()
+          },
+          {
+            id: 3,
+            description: 'Become React',
+            done: false,
+            targetDate: new Date()
+          }
+        ]
+    }
+  }
+
+
+  render() {
+    // The parameter from the URL will be read 
+    return (
+      <div>
+        <h1>List Todos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>description</th>
+              <th>done</th>
+              <th>is completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.todos.map(
+                todo =>
+                  <tr>
+                    <td>{todo.id}</td>
+                    <td>{todo.description}</td>
+                    <td>{todo.done.toString()}</td>
+                    <td>{todo.targetDate.toString()}</td>
+                  </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
 
 
 export default TodoApp;
