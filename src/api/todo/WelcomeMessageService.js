5317cf5fde5836769df8938015f0efb0ca7b8d1b
knuspersnack <thumos@posteo.gr>
2021-02-27 22:43:39 +0100

    CORS problems remain

--- src/api/todo/WelcomeMessageService.js
+++ src/api/todo/WelcomeMessageService.js
@@ -2,21 +2,9 @@ import axios from "axios";
 
 class WelcomeMessageService {
     getWelcomeMessage(name) {
-        let username = 'user';
-        let password = 'test';
 
-        //windows btoa ecodes the credentials using base64 (base 64 encoding)
-        let basicAuthHeader = 'Basic ' + window.btoa(`${username}:${password}`);
-        //let basicAuthHeader = 'Basic dXNlcjp0ZXN0';
-        console.log(basicAuthHeader);
 
-        return axios.get('http://localhost:8080/welcome/message/in28minutes',
-            {
-                headers: {
-                    'Authorization': basicAuthHeader,
-                }
-            }
-        );
+        return axios.get('http://localhost:8080/welcome/message/in28minutes');
     }
 }
 
--- src/service/AuthenticationService.js
+++ src/service/AuthenticationService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 class AuthenticationService {
     // ! remove or fix
     // authUserKey = 'authenticatedUser';
@@ -5,6 +7,7 @@ class AuthenticationService {
     registerSuccessfulLogin(username, password) {
         console.log("User logged in successfully!")
         sessionStorage.setItem('authenticatedUser', username);
+        this.setupAxiosInterceptors();
     }
 
     logout() {
@@ -13,13 +16,32 @@ class AuthenticationService {
 
     isUserLoggedIn() {
         let user = sessionStorage.getItem('authenticatedUser')
-        return (user!==null);
+        return (user !== null);
     }
 
     getLoggedInUserName() {
         let user = sessionStorage.getItem('authenticatedUser')
-        if(user===null) return '';
+        if (user === null) return '';
         return user;
+    }
+
+    //Installs an Interceptor, that adds a auth header to every outgoing requests
+    setupAxiosInterceptors() {
+        let username = 'user';
+        let password = 'test';
+        //windows btoa ecodes the credentials using base64 (base 64 encoding)
+        let basicAuthHeader = 'Basic ' + window.btoa(`${username}:${password}`);
+
+        console.log("Installing Axios Auth Interceptor..")
+
+        axios.interceptors.request.use(
+            (config) => {
+                if (this.isUserLoggedIn()) {
+                    config.headers.authorization = basicAuthHeader;
+                }
+                return config;
+            }
+        )
     }
 }
 
